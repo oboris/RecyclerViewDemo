168be6a8976ebf8c3f43266748b13948876c2935
Boris Onyshchenko <oboris@ukr.net>
2020-11-04 22:19:25 +0200

    move getDB from helper to DataBase and delete helper class

--- app/src/main/java/com/example/recyclerviewdemo/database/PersonDataBase.java
+++ app/src/main/java/com/example/recyclerviewdemo/database/PersonDataBase.java
@@ -1,13 +1,29 @@
 package com.example.recyclerviewdemo.database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.example.recyclerviewdemo.dao.PersonDAO;
 import com.example.recyclerviewdemo.model.PersonAddress;
 import com.example.recyclerviewdemo.model.PersonPhone;
 
-@Database(entities = {PersonPhone.class, PersonAddress.class}, version = 1)
+@Database(entities = {PersonPhone.class, PersonAddress.class}, version = 2, exportSchema = false)
 public abstract class PersonDataBase extends RoomDatabase {
+
+    private static final String PERSON_DATA_BASE_NAME = "person_database";
+
+    private static PersonDataBase instance;
+
     public abstract PersonDAO personDAO();
+
+    public static PersonDataBase getDB(Context context){
+        if (instance == null){
+            instance = Room.databaseBuilder(context, PersonDataBase.class, PERSON_DATA_BASE_NAME)
+                    .fallbackToDestructiveMigration().build();
+        }
+        return instance;
+    }
 }
