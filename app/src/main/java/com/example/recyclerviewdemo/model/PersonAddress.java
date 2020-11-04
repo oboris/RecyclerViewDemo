7b34ae72398c488a4aa70c76c9185606485f9d23
Boris Onyshchenko <oboris@ukr.net>
2020-11-04 14:03:44 +0200

    added annotation @Entity, @PrimaryKey

--- app/src/main/java/com/example/recyclerviewdemo/model/PersonAddress.java
+++ app/src/main/java/com/example/recyclerviewdemo/model/PersonAddress.java
@@ -1,8 +1,15 @@
 package com.example.recyclerviewdemo.model;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "person_address")
 public class PersonAddress implements IPerson{
     private String name;
     private String address;
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
 
     public PersonAddress(String name, String address) {
         this.name = name;
--- app/src/main/java/com/example/recyclerviewdemo/model/PersonPhone.java
+++ app/src/main/java/com/example/recyclerviewdemo/model/PersonPhone.java
@@ -1,8 +1,15 @@
 package com.example.recyclerviewdemo.model;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "person_phones")
 public class PersonPhone implements IPerson {
     private String name;
     private String phone;
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
 
     public PersonPhone(String name, String phone) {
         this.name = name;
